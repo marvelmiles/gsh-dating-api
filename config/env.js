52a175793e22059fa692a5e82aebec0a820a4eab
marvelmiles <marvellousabidemi2@gmail.com>
2026-09-05 07:18:47 +0100

    pre-seed config

--- config/env.js
+++ config/env.js
@@ -2,6 +2,8 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+// export const isProductionEnvironment = true;
+
 export const isProductionEnvironment =
   process.env.NODE_ENV === "production" ||
   process.env.ENVIRONMENT === "production";
